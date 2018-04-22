abeb9f078512ce7d042a25f67f517d0f49fffa78
Massimo Siboldi <mdsiboldi@gmail.com>
2018-04-22 16:38:48 -0700

    Organize and slightly document

--- main.lisp
+++ main.lisp
@@ -10,36 +10,15 @@
 (define new-grid (.> anim8 :newGrid))
 
 (define scale 2)
-(define foods :mutable '())
+
+; game objects
 
 (define world :mutable nil)
 (define canvas :mutable nil)
-(define dog :mutable nil)
-
-(define sheets {
-  :doughnut (love/graphics/new-image "assets/strawberry.png")
-  :dog (love/graphics/new-image "assets/dog-e-dog.png")
-})
-
-(define grids {
-  :doughnut (new-grid
-   16
-   16
-   (self (.> sheets :doughnut) :getWidth)
-   (self (.> sheets :doughnut) :getHeight))
-  :dog (new-grid
-   48
-   48
-   (self (.> sheets :dog) :getWidth)
-   (self (.> sheets :dog) :getHeight))
-})
-
-(define frames {
-  :doughnut (self (.> grids :doughnut) :getFrames "1-4" 1)
-  :dog (self (.> grids :dog) :getFrames "1-5" 1)
-})
-
 (define ground :mutable nil)
+(define dog :mutable nil)
+(define foods :mutable '())
+
 (defun new-ground ()
   (let* [(body (love/physics/new-body world (/ 800 2) (/ 750 2) "static"))
          (shape (love/physics/new-rectangle-shape 800 50))
@@ -55,11 +34,63 @@
    (self fixture :setUserData :dog)
    { :body body :shape shape :fixture fixture :anim anim }))
 
+(defun draw-dog (dog)
+  (self (.> dog :anim) :draw
+        (.> sheets :dog)
+        (self (.> dog :body) :getX)
+        (self (.> dog :body) :getY)
+        (self (.> dog :body) :getAngle)
+        1
+        1
+        15
+        30))
+
+(defun new-doughnut ()
+  (let* [
+    (body (love/physics/new-body world 300 300 "dynamic"))
+    (shape (love/physics/new-circle-shape 8))
+    (fixture (love/physics/new-fixture body shape))
+    (anim (new-animation (.> frames :doughnut) 0.1))
+    ]
+    (self body :applyLinearImpulse -8 -10)
+    (self fixture :setUserData :food)
+    (self body :setAngularVelocity 0.1)
+    { :body body :shape shape :fixture fixture :anim anim })
+  )
+
+
+; anim8 assets: sheets, grids n frames
+(define sheets {
+  :doughnut (love/graphics/new-image "assets/taco.png")
+  :dog (love/graphics/new-image "assets/dog-e-dog.png")
+  })
+
+(define grids {
+  :doughnut (new-grid
+             16
+             16
+             (self (.> sheets :doughnut) :getWidth)
+             (self (.> sheets :doughnut) :getHeight))
+  :dog (new-grid
+        48
+        48
+        (self (.> sheets :dog) :getWidth)
+        (self (.> sheets :dog) :getHeight))
+  })
+
+(define frames {
+  :doughnut (self (.> grids :doughnut) :getFrames "1-4" 1)
+  :dog (self (.> grids :dog) :getFrames "1-5" 1)
+  })
+
+
+; helpers
 (defun bounded (val min max)
   (cond [(> val max) max]
         [(< val min) min]
         [true val]))
 
+; sets angle on a love.physics body, between the min and max angles (in radians)
 (defun set-angle (body angle-delta min-angle max-angle)
   (let* [(old-angle (self body :getAngle))
         (new-angle (bounded (+ old-angle angle-delta)
@@ -68,23 +99,11 @@
    (when (/= old-angle new-angle)
      (self body :setAngle new-angle))))
 
-(defun new-doughnut ()
-  (let* [
-    (body (love/physics/new-body world 300 300 "dynamic"))
-    (shape (love/physics/new-circle-shape 8))
-    (fixture (love/physics/new-fixture body shape))
-    (anim (new-animation (.> frames :doughnut) 0.1))
-  ]
-    (self body :applyLinearImpulse -8 -10)
-    (self fixture :setUserData :food)
-    (self body :setAngularVelocity 0.1)
-    { :body body :shape shape :fixture fixture :anim anim })
-)
-
 (defun collision-with? (sym-a sym-b test-a test-b)
   (or (and (= sym-a test-a) (= sym-b test-b))
       (and (= sym-a test-b) (= sym-b test-a))))
 
+; callback for collision detection
 (defun begin-contact (a b coll)
   (let [(a-body (self a :getBody))
         (a-data (self a :getUserData))
@@ -96,9 +115,11 @@
 (defevent :load ()
   (love/window/set-mode 800 800 { :display 2 })
   (love/physics/set-meter 64)
+
   (set! world (love/physics/new-world 0 (* 9.81 64) true))
   (set! ground (new-ground))
   (set! dog (new-dog))
+
   (set! canvas (love/graphics/new-canvas 800 800))
   (self canvas :setFilter "nearest" "nearest")
 
@@ -110,6 +131,7 @@
   (self world :update dt)
   (self (.> dog :anim) :update dt)
 
+  ; update food list, removing and destroying objects marked for destruction
   (set! foods
         (reduce (lambda (foods food)
                   (self (.> food :anim) :update dt)
@@ -119,6 +141,7 @@
                 '()
                 foods))
 
+  ; keys
   (when (love/keyboard/is-down "space")
     (set! foods (cons (new-doughnut) foods)))
   (when (love/keyboard/is-down "w")
@@ -134,22 +157,12 @@
   (when (love/keyboard/is-down "e")
     (set-angle (.> dog :body) 0.1 -1 1)))
 
-(defun draw-dog (dog)
-  (self (.> dog :anim) :draw
-        (.> sheets :dog)
-        (self (.> dog :body) :getX)
-        (self (.> dog :body) :getY)
-        (self (.> dog :body) :getAngle)
-        1
-        1
-        15
-        30))
-
 (defevent :draw ()
   (love/graphics/set-canvas canvas)
   (love/graphics/clear)
   (draw-dog dog)
 
+  ; just draw all foods
   (do [(food foods)]
     (let* [(body (.> food :body))
            (anim (.> food :anim))

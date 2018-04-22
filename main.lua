7db6748b57e28349eae502c516065f301d5087ac
Eric Buckthal <eric@simplecontacts.com>
2018-04-22 13:42:12 -0700

    remove main.lua

--- main.lua
+++
@@ -1,6 +0,0 @@
--- A shim to bridge fennel and love2d
-fennel = require("fennel")
-table[("insert")](package[("loaders")], fennel[("searcher")])
-pp = function(x) print(require("fennelview")(x)) end
-
-fennel.dofile("main.fnl")
